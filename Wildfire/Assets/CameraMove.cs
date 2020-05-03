76d9e1b360203f6d78b8da4d7102894a0a6627d7
Craig Dunk <craig@goproperly.com>
2020-05-03 18:14:01 -0400

    feat: initial project with nav and prefab hex

---
+++ Wildfire/Assets/CameraMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+     
+    void Update()
+    {
+        Vector3 oldPos = transform.position;
+        if(Input.GetKey(KeyCode.RightArrow))
+        {      
+            transform.position = new Vector3(speed * Time.deltaTime,0,0) + oldPos;
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(-speed * Time.deltaTime,0,0) + oldPos;
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = new Vector3(0,0,speed * Time.deltaTime) +oldPos;
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = new Vector3(0,0,-speed * Time.deltaTime)+oldPos;
+        }
+    }
+}
+
---
+++ Wildfire/Assets/InitializeTerrain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializeTerrain : MonoBehaviour
+{
+    public GameObject hexTileTemplate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello from Craig");
+
+        Vector3 newPosition = new Vector3(5,0,0);
+
+        GameObject newHex = Instantiate(hexTileTemplate);
+
+        newHex.transform.position = newPosition;
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+//HexTile_Sheet_1A_3
+//X 90
