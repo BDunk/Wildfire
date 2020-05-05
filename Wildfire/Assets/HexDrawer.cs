66bf9fc26f847a560cfbc119ec2da63e81a6cace
benjamin dunk <benkdunk@gmail.com>
2020-05-04 21:34:19 -0400

    added BoardCreator thing

---
+++ Wildfire/Assets/HexDrawer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexDrawer : MonoBehaviour
+{
+    public int BoardSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int max = (BoardSize-1)/2;
+
+        for (int ii=-max;ii<=max/2;ii++)
+        {
+            Debug.Log("\n");
+            for (int jj=-max;jj<=max;jj++)
+            {
+                Debug.Log(ii.ToString()+", "+jj.ToString()+"    ");
+                
+            }
+                
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
