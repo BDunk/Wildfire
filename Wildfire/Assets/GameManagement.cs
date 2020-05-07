c19fccfff54a9269febc2f6fb5212c0685d4a3cc
Craig Dunk <craig@goproperly.com>
2020-05-06 22:06:22 -0400

    fix: missed files

---
+++ Wildfire/Assets/GameManagement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+class PlayerState {
+    public string name;
+}
+
+
+public class GameManagement : MonoBehaviour
+{
+    public GameObject mNavFrame;
+
+    private List<PlayerState> mPlayers = new List<PlayerState>();
+    private int mCurrentPlayerOffset = -1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        PlayerState player = null;
+        player = new PlayerState();
+        player.name = "Bob";
+        mPlayers.Add(player);
+        player = new PlayerState();
+        player.name = "Mai";
+        mPlayers.Add(player);
+
+        mCurrentPlayerOffset = 0;
+
+        SendStateToNavFrame();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HandleDoneTurn()
+    {
+        Debug.Log("Hello from Craig in done turn");
+        mCurrentPlayerOffset = (mCurrentPlayerOffset + 1) % (mPlayers.Count);
+        SendStateToNavFrame();
+
+
+    }
+
+    private void SendStateToNavFrame()
+    {
+        PlayerState currentPlayer = mPlayers[mCurrentPlayerOffset];
+
+        mNavFrame.BroadcastMessage("handlePlayerNameUpdate", currentPlayer.name );
+    }
+
+}
